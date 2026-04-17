800b296023926b74c7f04bd153742641b91bb3af
wangyao <2276606016@qq.com>
2026-04-17 10:44:55 +0800

    添加账号登录;文章发布;文章分类,标签

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,41 +1,57 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import PostDetail from '../views/PostDetail.vue'
-import Admin from '../views/Admin.vue'
-import CreatePost from '../views/CreatePost.vue'
-import EditPost from '../views/EditPost.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue'),
   },
   {
     path: '/post/:id',
     name: 'PostDetail',
-    component: PostDetail
+    component: () => import('../views/PostDetail.vue'),
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue'),
   },
   {
     path: '/admin',
     name: 'Admin',
-    component: Admin
+    component: () => import('../views/Admin.vue'),
+    meta: { requiresAuth: true },
   },
   {
-    path: '/admin/create',
+    path: '/create',
     name: 'CreatePost',
-    component: CreatePost
+    component: () => import('../views/CreatePost.vue'),
+    meta: { requiresAuth: true },
   },
   {
-    path: '/admin/edit/:id',
+    path: '/edit/:id',
     name: 'EditPost',
-    component: EditPost
-  }
+    component: () => import('../views/EditPost.vue'),
+    meta: { requiresAuth: true },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+})
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  } else if (to.path === '/login' && token) {
+    next('/admin')
+  } else {
+    next()
+  }
 })
 
 export default router
